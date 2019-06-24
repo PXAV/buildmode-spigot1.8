2ef086f36b1febb08d3f4c7232aa7f8682999d3a
pxav <pxav.studios@gmail.com>
2019-06-24 17:49:29 +0200

    Initial commit

---
+++ src/main/java/de/pxav/buildmode/BuildMode.java
@@ -0,0 +1,12 @@
+package de.pxav.buildmode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * A class description goes here.
+ *
+ * @author pxav
+ */
+public class BuildMode extends JavaPlugin {
+
+}
